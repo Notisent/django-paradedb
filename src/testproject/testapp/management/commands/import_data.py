eec8658fe7e92f881e75e9b82e5844f810bc9b04
Marco Bonetti <marco@cruncher.ch>
2025-04-02 20:34:22 +0200

    chore: more efficient way of importing the goodreads dataset

--- src/testproject/testapp/management/commands/import_data.py
+++ src/testproject/testapp/management/commands/import_data.py
@@ -2,7 +2,7 @@ import json
 import os
 
 import tqdm
-
+import fileinput
 from django.core.management.base import BaseCommand, CommandError
 
 from ...models import Book
@@ -10,7 +10,7 @@ from ...models import Book
 
 class Command(BaseCommand):
     def add_arguments(self, parser):
-        parser.add_argument("--path", type=str)
+        parser.add_argument("--path", type=str, required=True)
 
     def handle(self, **options):
         print("Reading file")
@@ -22,37 +22,38 @@ class Command(BaseCommand):
                 "gunzip it and point --path to the decompressed json "
                 "file"
             )
-        with open(options.get("path"), "r") as f:
-            t = f.read()
-            rows = t.splitlines()
 
         Book.objects.all().delete()
+        line_count = sum(1 for line in fileinput.input(options.get("path")))
 
-        books = []
-        i = 0
-        for row_ in tqdm.tqdm(rows):
-            row = json.loads(row_)
-            language_code = row.get("language_code")
-            if language_code.lower() in ("eng", "en-gb", "en-us", "en-ca", "en-au"):
-                i += 1
-                books.append(
-                    Book(
-                        title=row.get("title")[:255],
-                        isbn=row.get("isbn13", row.get("isbn", "")),
-                        average_rating=row.get("average_rating") or None,
-                        ratings_count=row.get("ratings_count", 0) or 0,
-                        description=row.get("description"),
-                        url=row.get("url", row.get("link", "")),
-                        image_url=row.get("image_url"),
-                        pages=row.get("num_pages") or None,
-                        publication_year=row.get("publication_year") or None,
-                        ext_id=row.get("book_id"),
+        prog = tqdm.tqdm(total=line_count)
+        with open(options.get("path"), "r") as f:
+            i = 0
+            books = []
+            while line := f.readline():
+                prog.update(1)
+                row = json.loads(line)
+                language_code = row.get("language_code")
+                if language_code.lower() in ("eng", "en-gb", "en-us", "en-ca", "en-au"):
+                    i += 1
+                    books.append(
+                        Book(
+                            title=row.get("title")[:255],
+                            isbn=row.get("isbn13", row.get("isbn", "")),
+                            average_rating=row.get("average_rating") or None,
+                            ratings_count=row.get("ratings_count", 0) or 0,
+                            description=row.get("description"),
+                            url=row.get("url", row.get("link", "")),
+                            image_url=row.get("image_url"),
+                            pages=row.get("num_pages") or None,
+                            publication_year=row.get("publication_year") or None,
+                            ext_id=row.get("book_id"),
+                        )
                     )
-                )
-                if i >= 10_000:
-                    Book.objects.bulk_create(books)
-                    books = []
-                    i = 0
+                    if i >= 10_000:
+                        Book.objects.bulk_create(books)
+                        books = []
+                        i = 0
 
-        if books:
-            Book.objects.bulk_create(books)
+            if books:
+                Book.objects.bulk_create(books)
